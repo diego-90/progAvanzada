734d5f5d1ae24b47c68b24e443e270b190b679b2
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-10-05 18:39:21 -0500

    Add files via upload

---
+++ ejercicio15.py
@@ -0,0 +1,7 @@
+ft = int(input('inserte pies:'))
+pulgadas = (ft*12)/1
+print('En in son:',pulgadas)
+yardas = (ft*0.333333)/1
+print('En ft son:',yardas)
+milla = (ft*0.000189394)/1
+print('En millas son:',milla)
