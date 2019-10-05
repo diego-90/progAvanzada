7ccdb6cd96068cfe913f64373aa39a125276690e
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-10-05 17:14:20 -0500

    Add files via upload

---
+++ ejercicio11.py
@@ -0,0 +1,7 @@
+
+eu = float(input('la eficiencia en Estados unidos de MPG es: '))
+
+convercion = 235.21 / eu 
+
+print('\n La eficiencia de conbustible en Mexico es: %.2f' % convercion, 'L/100km' )
+
