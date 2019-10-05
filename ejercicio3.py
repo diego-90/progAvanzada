8610868f6af9a6c81856d2ec9460ae69f2a431b3
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-10-05 16:35:22 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,6 @@
+largo = float(input('¿cual es la largo de la habitacion:'))
+ancho = float(input('cual es el ancho de la habitacion:'))
+print('el area de la habitacion es', largo*ancho, 'metros cuadrados')
+
+
+
