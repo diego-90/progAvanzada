56a1d9a828335f0cac61e8d7d2bd98dade4edf1e
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-10-05 17:03:37 -0500

    Add files via upload

---
+++ ejercicio9.py
@@ -0,0 +1,10 @@
+dinero = int(input('introduzca la cantidad de dinero depositada en el primer año $:'))
+interes =(dinero * 0.04)
+suma1=dinero+interes
+print('primer año',suma1)
+interes2=suma1*0.04
+suma2=suma1+interes2
+print('segundo año', suma2)
+interes3=suma2*0.04
+suma3=suma2+interes3
+print('tercer año',suma3)
