c7f4d13a976c21ede3abd8ac0e27769a65ddabfe
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-10-05 17:01:03 -0500

    Add files via upload

---
+++ ejercicio8.py
@@ -0,0 +1,6 @@
+cornflakes = int(input('introdusca cuantas cajas de cornflakes tienes:'))
+trix = int(input('introdusca cuantas cajas de trix tienes:'))
+peso1 = 750*cornflakes
+peso2 = 500*trix
+pesototal = (peso1+peso2)/1000
+print('el peso total es',pesototal,'kg')
