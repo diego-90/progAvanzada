d93c8c276f7899ae883150e04a5f384761760841
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-11-10 13:17:19 -0600

    Add files via upload

---
+++ ejerciciowhile2.py
@@ -0,0 +1,12 @@
+a = 1
+sumatoria = 0
+i = 0
+
+while a != 0:
+    a = int(input('inserte el valor:'))
+    sumatoria = sumatoria + a
+    i = 1 + 1
+promedio = sumatoria / (i-1)
+print ('el promedio es:' , promedio)
+
+
