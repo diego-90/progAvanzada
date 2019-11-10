d83693ae7fa5e6f8a320d012726f5d3bab6d4363
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-11-10 13:28:18 -0600

    Add files via upload

---
+++ for5.py
@@ -0,0 +1,38 @@
+import turtle
+ventana=turtle.Screen ()
+ventana.bgcolor('white')
+ventana.title('mi ventana')
+
+rafael=turtle.Turtle()
+rafael.shape('turtle')
+rafael.pensize(1)
+rafael.speed(2)
+
+rafael.pendown()
+rafael.color('black')
+rafael.forward(700)
+rafael.left(180)
+rafael.forward(100)
+rafael.right(90)
+
+rafael.color('brown')
+rafael.forward(150)
+rafael.left(45)
+rafael.forward(60)
+
+
+rafael.color('green')
+rafael.forward(100)
+rafael.left(240)
+rafael.forward(50)
+rafael.left(240)
+fafael.right(240)
+rafael.forward(50)
+rafael.right(50)
+
+
+
+ventana.mainloop()
+
+
+
