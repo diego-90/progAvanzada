147f0469e30b9bd515ba5d9748e2f3a4b7a39156
diego-90 <55461583+diego-90@users.noreply.github.com>
2019-11-10 13:29:47 -0600

    Add files via upload

---
+++ evaluacion1.py
@@ -0,0 +1,9 @@
+factor1 = int(input('inserta un nymero entero:'))
+factor2 = int(input('inserta otro numero entero:'))
+multiplicacion = factor1*factor2
+print ('el producto es:', multiplicacion)
+if multiplicacion > 1000 :
+    suma = factor1+factor2
+    print ('la suma de los numeros es:',suma)
+elif multiplicacion < 1000 :
+    print ('el  producto es menor a 1000:')
